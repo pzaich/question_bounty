729bbd5527b2c226881a494da4fbcab8d04ddee4
Paul Zaich <pzaich@gmail.com>
2012-10-29 21:01:11 +0900

    contorller mini refactor:

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,14 +1,12 @@
 class AnswersController < ApplicationController
 	before_filter :authenticate_user!, :except => [:show]
-
+	before_filter :find_question
 	def new
-		@question = Question.find(params[:question_id])
 		@answer = @question.answers.new
 		3.times { @answer.arguments.build }
 	end
 
 	def create
-		@question = Question.find(params[:question_id])
 		answer_params = params[:answer].merge({ :question_id => params[:question_id] })
 		@answer = current_user.answers.new(answer_params)
 		if @answer.save
@@ -20,4 +18,8 @@ class AnswersController < ApplicationController
 		end
 	end
 
+	private
+		def find_question
+			@question = Question.find(params[:question_id])
+		end
 end

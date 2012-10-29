db6467ad74c408e2e0d34cb8239f0aab6ce14e30
Paul Zaich <pzaich@gmail.com>
2012-10-29 16:30:08 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'heroku'
+gem 'jquery-rails'
+gem 'pg'
+gem 'friendly_id'
+gem 'devise'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem "twitter-bootstrap-rails", '2.1.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuestionBounty::Application.config.secret_token = '00afbd6de106b1dcc55a5c279a9de9407a115b71c739b5f2f92104a672bfa0c54b9778c034aa8213527cc1533be48d610a26dc40bc14b699ea4a463471ec9d0c'

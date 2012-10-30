cbe22afa5ec348f393072b43b4ed6b805073b7cf
Paul Zaich <pzaich@gmail.com>
2012-10-30 15:35:33 +0900

    js voting on answers

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,11 @@
 class VotesController < ApplicationController
+
+	def create
+		@answer = Answer.find(params[:answer_id])
+		@vote = current_user.votes.new(:answer => @answer)
+		if @vote.save
+			render 'create'
+		else
+		end
+	end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,7 @@
 class Vote < ActiveRecord::Base
-  attr_accessible :answer_id, :user_id, :value
+  attr_accessible :answer, :user_id, :value
   belongs_to :answer
   belongs_to :user
   
+  validates_uniqueness_of :answer_id, :scope => :user_id
 end

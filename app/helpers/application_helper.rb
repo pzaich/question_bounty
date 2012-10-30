cf305e920f6c8603657c534b65e08a1a1a39ee7c
Paul Zaich <pzaich@gmail.com>
2012-10-30 11:41:28 +0900

    time expiration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def time_to_deadline(expiration)
+		time_left = expiration - Time.now
+		if time_left < 60
+			time_left.to_i.to_s + "seconds"
+		elsif time_left < 3600
+			(time_left / 60).to_i.to_s + "minutes"
+		else time_left < 86400
+			(time_left / 3600).to_i.to_s + "hours"
+		end
+	end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,13 @@
 class Question < ActiveRecord::Base
-  attr_accessible :description, :expiration, :title, :image_url
+  attr_accessible :description, :expiration, :title, :image_url, :hours
+  before_create :generate_deadline
+  attr_accessor :hours
   belongs_to :user
   has_many :answers
   has_many :arguments
 
+  def generate_deadline
+  	self.expiration = Time.now + self.hours.to_i.hours
+  end
   validates_presence_of :title
-  validates_presence_of :expiration
 end
